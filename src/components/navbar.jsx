7db3ed8b3034864c735e2d200fdde587352cf2f4
kourosh <kourosh0903@gmail.com>
2025-01-13 10:09:22 -0800

    added hero component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import logo from '../assets/images/logo.png';
 
-export const Navbar = () => {
+const Navbar = () => {
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -15,34 +15,35 @@ export const Navbar = () => {
                 src={logo}
                 alt="React Jobs"
               />
-              <span className="hidden md:block text-white text-2xl font-bold ml-2"
-                >React Jobs</span
-              >
+              <span 
+                className="hidden md:block text-white text-2xl font-bold ml-2">
+                React Jobs
+              </span>
             </a>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
                 <a
                   href="/index.html"
-                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Home</a
-                >
+                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                    Home
+                </a>
                 <a
                   href="/jobs.html"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Jobs</a
-                >
+                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                  Jobs
+                </a>
                 <a
                   href="/add-job.html"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >Add Job</a
-                >
+                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">
+                  Add Job
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
