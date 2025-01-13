5d2538ba084c841269d7af4944441b5e42e1fc97
kourosh <kourosh0903@gmail.com>
2025-01-13 11:01:31 -0800

    added card component

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,4 +1,4 @@
-const hero = () => {
+const Hero = () => {
   return (
     <section className="bg-indigo-700 py-20 mb-4">
       <div
@@ -19,4 +19,4 @@ const hero = () => {
   )
 }
 
-export default hero
+export default Hero;
