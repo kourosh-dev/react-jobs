85ea88ed21cba2df558cb296779f4b1c2a6d7dbe
kourosh <kourosh0903@gmail.com>
2025-01-14 03:20:01 -0800

    limit jobs to 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/viewAllJobs";
 
 const App = () => {
   return (
     <>
-    <Navbar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
     </>
   );
 };
--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,6 +2,8 @@ import Jobs from '../jobs.json'
 import JobListing from './JobListing';
 
 const JobListings = () => {
+  const recentJob = Jobs.slice(0, 3);
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -10,7 +12,7 @@ const JobListings = () => {
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-          {Jobs.map((job) => (
+          {recentJob.map((job) => (
             <JobListing job={job} key={job.id}/>
           ))}
 
