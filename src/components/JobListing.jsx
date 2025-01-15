3de80279ce4c8c70fc186841405a4fb623ef8fa7
kourosh <kourosh0903@gmail.com>
2025-01-15 11:49:58 -0800

    used useState to change job description

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,14 @@
-import JobListings from "./JobListings";
+import { useState } from 'react';
 
 const JobListing = ({job}) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = job.description;
+
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + '...';
+  }
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -9,9 +17,18 @@ const JobListing = ({job}) => {
           <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
 
-        <div className="mb-5">
-        {job.description}
-        </div>
+        <div className="mb-5">{description}</div>
+
+        <button 
+          onClick={() => {
+            setShowFullDescription((prevState) => {
+              return !prevState
+            })
+          }}
+          className="text-indigo-500 mb-5 hover:text-indigo-600"
+        >
+          {showFullDescription ? 'Less' : 'More'}
+        </button>
 
         <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
