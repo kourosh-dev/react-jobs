685415603df5b7fa26587f54caa84a9d9d113582
kourosh <kourosh0903@gmail.com>
2025-01-19 09:33:28 -0800

    made not found page and use route links

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ import {
 } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout.jsx';
 import HomePage from './pages/HomePage.jsx';
+import JobsPage from './pages/JobsPage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
     </Route>
   )
 );
