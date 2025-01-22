fafd839af02772608804bd1d261ab1f75a6d557f
kourosh <kourosh0903@gmail.com>
2025-01-22 02:00:14 -0800

    now you can edit job

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import JobsPage from './pages/JobsPage.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
 import JobPage, { jobLoader } from './pages/JobPage.jsx';
 import AddJobPage from './pages/AddJobPage.jsx';
+import EditJobPage from './pages/EditJobPage.jsx';
 
 const App = () => {
   // Add New Job
@@ -35,13 +36,41 @@ const App = () => {
     return;
   }
 
+  // Edit Job
+  async function updateJob(job) {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
+    });
+
+    return;
+  }
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
-        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
-        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+        <Route 
+          path='/add-job' 
+          element={<AddJobPage 
+            addJobSubmit={addJob} />
+          } 
+        />
+        <Route 
+          path='/edit-job/:id' 
+          element={<EditJobPage updateJobSubmit={updateJob} />} 
+          loader={jobLoader} 
+        />
+        <Route 
+          path='/jobs/:id' 
+          element={<JobPage deleteJob={deleteJob} />} 
+          loader={jobLoader} 
+        />
+        <Route />
         <Route path='*' element={<NotFoundPage />} />
       </Route>
     )
--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -1,24 +1,27 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { useParams, useLoaderData, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
-const AddJobPage = ({ addJobSubmit }) => {
-  const [title, setTitle] = useState('');
-  const [type, setType] = useState('Full-Time');
-  const [location, setLocation] = useState('');
-  const [description, setDescription] = useState('');
-  const [salary, setSalary] = useState('Under $50K');
-  const [companyName, setCompanyName] = useState('');
-  const [companyDescription, setCompanyDescription] = useState('');
-  const [contactEmail, setContactEmail] = useState('');
-  const [contactPhone, setContactPhone] = useState('');
-
+const EditJobPage = ({ updateJobSubmit }) => {
+  const job = useLoaderData();
   const navigate = useNavigate();
+  const { id } = useParams();
+
+  const [title, setTitle] = useState(job.title);
+  const [type, setType] = useState(job.type);
+  const [location, setLocation] = useState(job.location);
+  const [description, setDescription] = useState(job.description);
+  const [salary, setSalary] = useState(job.salary);
+  const [companyName, setCompanyName] = useState(job.company.name);
+  const [companyDescription, setCompanyDescription] = useState(job.company.description);
+  const [contactEmail, setContactEmail] = useState(job.company.contactEmail);
+  const [contactPhone, setContactPhone] = useState(job.company.contactPhone);
 
   function submitForm(e) {
     e.preventDefault();
 
-    const newJob = {
+    const updatedJob = {
+      id,
       title,
       type,
       location,
@@ -32,12 +35,12 @@ const AddJobPage = ({ addJobSubmit }) => {
       }
     }
 
-    addJobSubmit(newJob);
-    toast.success('Job Added Succeefully');
+    updateJobSubmit(updatedJob);
+    toast.success('Job Updated Succeefully');
 
-    return navigate('/jobs');
+    return navigate(`/jobs/${id}`);
   }
-  
+
   return (
     <section className="bg-indigo-50">
       <div className="container m-auto max-w-2xl py-24">
@@ -45,7 +48,7 @@ const AddJobPage = ({ addJobSubmit }) => {
           className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
         >
           <form onSubmit={submitForm}>
-            <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
+            <h2 className="text-3xl text-center font-semibold mb-6">Edit Job</h2>
 
             <div className="mb-4">
               <label htmlFor="type" className="block text-gray-700 font-bold mb-2">
@@ -217,7 +220,7 @@ const AddJobPage = ({ addJobSubmit }) => {
                 className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                 type="submit"
               >
-                Add Job
+                Edit Job
               </button>
             </div>
           </form>
@@ -227,4 +230,4 @@ const AddJobPage = ({ addJobSubmit }) => {
   )
 }
 
-export default AddJobPage;
+export default EditJobPage;
